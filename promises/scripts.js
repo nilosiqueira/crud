e737d5346b1736bd42f5e6d39071ff9e2659c24a
Nilo Siqueira <nilosiqueiratj@gmail.com>
2019-11-03 11:40:04 -0300

    primeiro commit

--- promises/scripts.js
+++ promises/scripts.js
@@ -3,7 +3,9 @@ const user = [
   { id: 2, name: 'Lucas', bonus: 400 },
   { id: 3, name: 'Hugo', bonus: 300 },
   { id: 4, name: 'Thiago', bonus: 200 },
-  { id: 5, name: 'Ronan', bonus: 100 }
+  { id: 5, name: 'Ronan', bonus: 100 },
+  { id: 6, name: 'Leonardo', bonus: 90 },
+  { id: 7, name: 'Bruno', bonus: 95 }
 ]
 
 const bonus = user.filter(users => {
