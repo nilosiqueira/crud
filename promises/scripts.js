428621745c0aae42e79ca0b9c058f66e0917dc65
Nilo Siqueira <nilosiqueiratj@gmail.com>
2019-11-03 11:15:17 -0300

    crud commit

---
+++ promises/scripts.js
@@ -0,0 +1,15 @@
+const user = [
+  { id: 1, name: 'Nilo', bonus: 500 },
+  { id: 2, name: 'Lucas', bonus: 400 },
+  { id: 3, name: 'Hugo', bonus: 300 },
+  { id: 4, name: 'Thiago', bonus: 200 },
+  { id: 5, name: 'Ronan', bonus: 100 }
+]
+
+const bonus = user.filter(users => {
+  return users.bonus <= 300
+})
+
+const usuarios = document.querySelector('user')
+
+console.log(bonus)
